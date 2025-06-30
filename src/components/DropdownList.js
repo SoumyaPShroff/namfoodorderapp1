5ff87c99c608044e8cc1d96eb8388aba60e63b28
SoumyaPShroff <bsoumyashri@gmail.com>
2025-06-30 17:05:50 +0530

    resList taken from json file instead at component level

--- src/components/DropdownList.js
+++ src/components/DropdownList.js
@@ -1,10 +1,22 @@
 import React from 'react'
 
-function DropdownList() {
-  return (
-    <div>
-      
-    </div>
+function DropdownList({ data = [], labelKey = "label", valueKey = "value", onChange, selectedValue, placeholder = "-- Select --" }, disabled = false) {
+return (
+    <select
+      value={selectedValue}
+      onChange={onChange}
+      disabled={disabled}
+      className="dropdown"
+    >
+      <option value="" disabled hidden>
+        {placeholder}
+      </option>
+      {data.map((item, index) => (
+        <option key={index} value={item[valueKey]}>
+          {item[labelKey]}
+        </option>
+      ))}
+    </select>
   )
 }
 
